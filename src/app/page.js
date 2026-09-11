aeb1c1b9e4f570af5d0f61f60083bec149986c04
AmmyAstro <amrender@dhwaniastro.com>
2026-09-11 18:03:51 +0530

    token token

--- src/app/page.js
+++ src/app/page.js
@@ -37,33 +37,37 @@ export default function StaffLogin() {
   const [showPassword, setShowPassword] = useState(false);
   const { connectSocket } = useContext(SocketContext);
   const [loginStaff, { loading }] = useMutation(LOGIN_STAFF, {
-onCompleted: async (data) => {
-  console.log("Login Response:", data);
+    onCompleted: async (data) => {
+      console.log("Login Response:", data);
 
-  const { accessToken, refreshToken, user } = data.loginStaff;
+      const { accessToken, refreshToken, user } = data.loginStaff;
 
-  console.log("Access Token:", accessToken);
-  console.log("Refresh Token:", refreshToken);
-  console.log("User:", user);
+      console.log("Access Token:", accessToken);
+      console.log("Refresh Token:", refreshToken);
+      console.log("User:", user);
 
-  authTokenVar(accessToken);
+      authTokenVar(accessToken);
 
-  localStorage.setItem("token", accessToken);
-  localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("token", accessToken);
+      localStorage.setItem("user", JSON.stringify(user));
 
-  console.log("LocalStorage Token:", localStorage.getItem("token"));
+      console.log("LocalStorage Token:", localStorage.getItem("token"));
+  document.cookie = `token=${accessToken}; path=/; max-age=${60 * 60 * 24 * 7}; SameSite=Lax`;
+      connectSocket({
+        adminId: user.id,
+        token: accessToken,
+      });
 
-  connectSocket({
-    adminId: user.id,
-    token: accessToken,
-  });
-
-  toast.success(`Welcome ${user.name}`);
-
-  await client.resetStore();
-
-  router.push("/Admindash");
-},
+      toast.success(`Welcome ${user.name}`);
+     const tokentoekn = localStorage.getItem("token");
+      await client.resetStore();
+      if(tokentoekn ){
+        console.log("qwertyuiokjhgfdsdcvbhjgfdsasdfghjhgfd");
+        
+        router.push("/Admindash")
+      }
+      
+    },
     onError: (err) => {
       toast.error(err.message || "Login failed");
     },
