cf95c9cf5b0ae6cb4497fbf8c31b9ac2b5f2ae9a
tejpal singh <tej@dhwaniastro.com>
2026-09-17 15:51:06 +0530

    change staging url

--- src/components/utils/apolloClient.js
+++ src/components/utils/apolloClient.js
@@ -21,14 +21,14 @@ export const authTokenVar = makeVar(null);
 const httpLink = new HttpLink({
   uri:
     process.env.NEXT_PUBLIC_GRAPHQL_URL ||
-    "https://dhwaniastro.com/adminAuth/graphql",
+    "https://staging.dhwaniastro.com/adminAuth/graphql",
   credentials: "include",
 });
 
 const uploadLink = new UploadHttpLink({
   uri:
     process.env.NEXT_PUBLIC_GRAPHQL_URL ||
-    "https://dhwaniastro.com/adminAuth/graphql",
+    "https://staging.dhwaniastro.com/adminAuth/graphql",
   credentials: "include",
   headers: {
     "apollo-require-preflight": "true",
